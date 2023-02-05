5361f792084607615fd1194e23deff42111013b4
Knarkzel <knarkzel@gmail.com>
2023-02-05 10:39:12 +0100

    Setup axum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use anyhow::Result;
-
+use axum_error::Result;
+use std::net::SocketAddr;
+use axum::{response::Html, routing::get, Router};
+ 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let body = reqwest::get("https://lukesmith.xyz").await?.text().await?;
-    println!("Result: {body}");
-    Ok(())
+async fn main() {
+    let app = Router::new().route("/", get(index));
+    axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 8000)))
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
+}
+ 
+async fn index() -> Html<String> {
+    Html(String::from("<h1>Hello there</h1>"))
 }
