b890f6848846f0855f722a30aaa6483694341b5c
Knarkzel <knarkzel@gmail.com>
2023-02-05 11:01:37 +0100

    todo: Fix database

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// Imports
+pub use axum_error::Result;
+
+// Templates
 pub use sailfish::TemplateOnce;
            
 pub mod template {
@@ -6,4 +10,30 @@ pub mod template {
     #[derive(TemplateOnce)]
     #[template(path = "index.html")]
     pub struct Index;
+}
+
+// Database
+use tokio_rusqlite::Connection;
+
+pub struct Database(Connection);
+
+impl Database {
+    pub async fn new() -> Result<Self> {
+        let connection = Connection::open_in_memory().await?;
+        Ok(Self(connection))
+    }
+}
+
+impl std::ops::Deref for Database {
+    type Target = Connection;
+    
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl std::ops::DerefMut for Database {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use barakah::*;
-use axum_error::Result;
-use std::net::SocketAddr;
 use sailfish::TemplateOnce;
+use std::{net::SocketAddr, sync::Arc};
 use axum::{response::Html, routing::get, Router};
- 
+
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(index));
+async fn main() -> Result<()> {
+    let app = Router::new()
+        .route("/", get(index))
+        .with_state(Arc::new(Database::new().await?));
+    
     axum::Server::bind(&SocketAddr::from(([127, 0, 0, 1], 8000)))
         .serve(app.into_make_service())
-        .await
-        .unwrap()
+        .await?;
+    Ok(())
 }
  
-async fn index() -> Result<Html<String>> {
-    Ok(Html(template::Index.render_once()?))
+async fn index(db: Database) -> Result<Html<String>> {
+    Ok(Html(template::Index.render_once()?)
 }
+
